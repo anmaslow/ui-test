b6cf7db69ad8f2306e61b36fc2c4c5d68ec98b3b
Anastasia <ann.maslow@gmail.com>
2020-01-26 23:00:03 +0300

    add validation test

--- src/test/java/ru/netology/SutTest.java
+++ src/test/java/ru/netology/SutTest.java
@@ -9,18 +9,50 @@ import org.openqa.selenium.WebDriver;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class SutTest {
+public class SutTest {
 
     @Test
     @DisplayName("Should submit request")
     void shouldSubmitRequest() {
-        open("http;//localhost:9999");
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
-        form.$("[data-test-id=name] input").setValue("Александр Петров");
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Петров Александр");
         form.$("[data-test-id=phone] input").setValue("+79051112233");
         form.$("[data-test-id=agreement]").click();
-        form.$("button button_view_extra button_size_m button_theme_alfa-on-white").click();
-        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена!"));
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    @DisplayName("Should validate empty form")
+    void shouldValidateEmptyForm() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    @DisplayName("Should check incorrect name")
+    void shouldCheckIncorrectName() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Ivanova Inna");
+        form.$("[data-test-id=phone] input").setValue("+79051112233");
+        form.$("button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    @DisplayName("Should check incorrect phone number")
+    void shouldCheckIncorrectPhoneNumber() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Александр Петров");
+        form.$("[data-test-id=phone] input").setValue("89051112233");
+        form.$("button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 }
